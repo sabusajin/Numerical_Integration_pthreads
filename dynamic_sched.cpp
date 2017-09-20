66a18ee0e88c2e3d281f279c181dff78c412b6ef
Sajin Sabu <sajinsabu1905@gmail.com>
2017-09-20 14:24:39 -0700

    first commit

---
+++ dynamic_sched.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+  
+int main (int argc, char* argv[]) {
+
+  if (argc < 9) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <sync> <granularity>"<<std::endl;
+    return -1;
+  }
+
+  return 0;
+}
---
+++ static_sched.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+  
+int main (int argc, char* argv[]) {
+
+  if (argc < 8) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <sync>"<<std::endl;
+    return -1;
+  }
+
+  
+  return 0;
+}
